7e420ed191a8bf9d92eeee7a70979e9bcbfc0876
Cherise Stanley <61471777+catstacks@users.noreply.github.com>
2021-12-12 22:33:47 +0000

    initia; commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Computed & Booted`,
+    author: `catstacks`,
+    description: `A data science, software development and general careers blog for budding computer enthusiasts.`,
+    siteUrl: `https://computed-and-booted.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `catstacks`,
     },
   },
   plugins: [
